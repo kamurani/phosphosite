32a5c1e991dc2aed5e7d0b97571754fb2afa3145
kamurani <cameronmcmenamie@gmail.com>
2023-05-05 15:17:39 +1000

    fix typo

--- phosphosite/__init__.py
+++ phosphosite/__init__.py
@@ -7,7 +7,7 @@ PROJECT_ROOT_DIR = Path(__file__).resolve().parent.parent
 PHOSPHOSITE_DIR = PROJECT_ROOT_DIR / 'phosphosite'
 DATA_DIR        = PROJECT_ROOT_DIR / 'data'
 
-STRUCTURE_DIR   = DATA_DIR / 'structure'
+STRUCTURE_DIR   = DATA_DIR / 'structures'
 CIF_DIR         = STRUCTURE_DIR / 'alphafold' / 'cif'
 PAE_DIR         = STRUCTURE_DIR / 'alphafold' / 'pae'
 PDB_DIR         = STRUCTURE_DIR / 'alphafold' / 'pdb' # PDB format structures from AF database.
