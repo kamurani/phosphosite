0bf7b664a576c3c0cb7b868276441d5945d88922
kamurani <cameronmcmenamie@gmail.com>
2023-06-12 13:55:14 +1000

    add logo todos

---
+++ phosphosite/logo/__init__.py
@@ -0,0 +1,11 @@
+"""Motif logo generator."""
+
+
+# TODO: 
+# use statistical information to determine structural motif logo
+# will have to define a 'structural motif' as the set of all bubbles 
+# for a given kinase (known through matching PSP dataset)
+# then, for each bubble, determine the probability of a given amino acid
+# being in that bubble, which scales each residue (i.e. the '3rd nearest residue') 
+# scale with information content. 
+
