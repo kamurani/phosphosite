80edb4f5fcd5a7ad5d7e8aeb053df736e5b3d9b2
kamurani <cameronmcmenamie@gmail.com>
2023-09-14 18:32:44 +1000

    add masked f1 score

--- phosphosite/ml/__init__.py
+++ phosphosite/ml/__init__.py
@@ -1,6 +1,8 @@
 """ML utilities."""
 
 import torch
+
+from sklearn.metrics import f1_score 
 from torchmetrics import Accuracy
 accuracy = Accuracy(task="binary")
 
@@ -35,6 +37,45 @@ def calculate_masked_accuracy(
     acc = accuracy(input[mask_idx], target[mask_idx])
     return acc
     
+def calculate_masked_f1(
+    input: torch.Tensor,
+    target: torch.Tensor,
+    mask: torch.Tensor,
+    **kwargs,
+) -> float:
+    """
+    Calculate the F1 score of the model's predictions.
+
+    Only considers the predictions for the masked residues, 
+    i.e. where the mask is nonzero. 
+
+    Parameters
+    ----------
+    input : torch.Tensor
+        The model's predictions.
+    target : torch.Tensor
+        The target values.
+    mask : torch.Tensor
+        The mask tensor.
+    **kwargs
+        Keyword arguments to pass to `sklearn.metrics.f1_score`.
+
+    Returns
+    -------
+    float
+        The F1 score.
+
+    """
+    y_pred =  (input > 0.5).int()
+    indexes = torch.nonzero(mask, as_tuple=True)[0] 
+
+    def get_masked(tensor):
+        tensor = tensor[indexes]
+        return tensor.detach().cpu().numpy()
+    
+    y        = get_masked(target)
+    y_pred   = get_masked(y_pred) 
+    return f1_score(y, y_pred, **kwargs)
 
 
 class MaskedMSELoss(torch.nn.Module):
