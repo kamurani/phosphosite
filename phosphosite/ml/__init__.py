b667cd44a005d74f5e45bfc9ddb662115ebba670
kamurani <cameronmcmenamie@gmail.com>
2023-09-19 11:44:35 +1000

    fix typo

--- phosphosite/ml/__init__.py
+++ phosphosite/ml/__init__.py
@@ -2,7 +2,7 @@
 
 from .aggr import MaskedAggregation
 from .metrics import calculate_masked_accuracy, calculate_masked_f1
-from .loss import MaskedBinaryCrossEntropy, MaskedBCEWithLogitsLoss
+from .loss import MaskedBinaryCrossEntropy, MaskedMSELoss
 
 
 
